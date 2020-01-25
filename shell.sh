678afa8c7d9b5f0464ad1ad354de68157dd6a6db
mineshmane <mineshmane94@gmail.com>
2020-01-25 17:18:58 +0530

    Added:each month lost and win count

--- shell.sh
+++ shell.sh
@@ -5,9 +5,14 @@ DAY=1;
 BET=1;
 WIN=1;
 LOSS=0;
-DAYS=20;
+DAYS=30;
 PERCENTAGE_STAKE=50;
+winCount=0;
+winTotalAmount=0;
+loassTotalAmount=0;
+lossCount=0;
 #finalStake=$INITIAL_STAKE;
+declare -A gamblerAmount
 winCondition=$(( $INITIAL_STAKE + $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
 loosCondition=$(( $INITIAL_STAKE - $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
 function gamblerSimulator(){
@@ -30,9 +35,26 @@ do
 	totalAmount=$(( $finalStake - $INITIAL_STAKE ))
    echo $totalAmount
 	totalGamblerAmount=$(( $totalGamblerAmount + $totalAmount ))	
-	echo $totalGamblerAmount
+gamblerAmount[$dayCount]=$totalAmount
+		echo $totalGamblerAmount
 done
 	echo "leavefor the day"
+	for data in "${!gamblerAmount[@]}"
+	do
+		if [[ ${gamblerAmount[$data]} -lt $LOSS ]]
+		then
+			lossCount=$(( $lossCount + 1 ))
+			lossTotalAmount=$(( $lossTotalAmount + ${gamblerAmount[$data]} ))
+			
+		else
+			winCount=$(( $winCount + 1 ));
+			winTotalAmount=$(( $winTotalAmount + ${gamblerAmount[$data]} ))
+
+		fi
+
+	done
+		echo "loss Count:" $lossCount  "lossAmount:" $lossTotalAmount
+		echo "win count :" $winCount "winAmount:"  $winTotalAmount
 }
 
 gamblerSimulator $(($WIN))
