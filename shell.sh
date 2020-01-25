c29450aa2d97e941dc1bf17fc4e4873b5fa49f29
mineshmane <mineshmane94@gmail.com>
2020-01-25 12:46:03 +0530

    Added:UC2 gambler 50% won lost

--- shell.sh
+++ shell.sh
@@ -5,9 +5,13 @@ DAY=1;
 BET=1;
 WIN=1;
 LOSS=0;
-finalStake=INITIAL_STAKE;
+PERCENTAGE_STAKE=50;
+finalStake=$INITIAL_STAKE;
+winCondition=$(( $INITIAL_STAKE + $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
+loosCondition=$(( $INITIAL_STAKE - $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
 function GamblerSimulator(){
-
+while [[ $finalStake <  $winCondition  &&  $finalStake > $loosCondition ]]
+do
 	randomNumberBet=$((RANDOM%2))
 	if [[ $randomNumberBet -eq $1 ]]
 	then
@@ -17,6 +21,8 @@ function GamblerSimulator(){
 		finalStake=$(( $finalStake - 1 ))
 		return $finalStake
 	fi
+done
+	echo "leavefor the day"
 }
 
 GamblerSimulator $(($WIN))
