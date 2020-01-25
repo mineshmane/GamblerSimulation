a37d39184e5cee55a0a24ba9c2b25bed410155fe
mineshmane <mineshmane94@gmail.com>
2020-01-25 15:27:15 +0530

    Added:UC3 for 20 days total loss and win

--- shell.sh
+++ shell.sh
@@ -5,24 +5,32 @@ DAY=1;
 BET=1;
 WIN=1;
 LOSS=0;
+DAYS=20;
 PERCENTAGE_STAKE=50;
 finalStake=$INITIAL_STAKE;
 winCondition=$(( $INITIAL_STAKE + $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
 loosCondition=$(( $INITIAL_STAKE - $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
-function GamblerSimulator(){
+function gamblerSimulator(){
+for (( dayCount=1; dayCount<=$DAYS; dayCount++ ))
+	do
+	finalStake=$INITIAL_STAKE;
 while [[ $finalStake <  $winCondition  &&  $finalStake > $loosCondition ]]
 do
-	randomNumberBet=$((RANDOM%2))
-	if [[ $randomNumberBet -eq $1 ]]
-	then
-		finalStake=$(( $finalStake + 1 ))
-		return $finalStake
-	else
-		finalStake=$(( $finalStake - 1 ))
-		return $finalStake
-	fi
+
+		randomNumberBet=$((RANDOM%2))
+		if [[ $randomNumberBet -eq $1 ]]
+		then
+			finalStake=$(( $finalStake + 1 ))
+			return $finalStake
+		else
+			finalStake=$(( $finalStake - 1 ))
+			return $finalStake
+		fi
+	done
+totalAmount=$(( $finalStake - $INITIAL_STAKE ))
+	totalGamblerAmount=$(( $totalGamblerAmount + $totalAmount ))	
 done
 	echo "leavefor the day"
 }
 
-GamblerSimulator $(($WIN))
+gamblerSimulator $(($WIN))
