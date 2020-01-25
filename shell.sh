2de26f219d3ea5d1f6a18087290d7d370f1832f0
mineshmane <mineshmane94@gmail.com>
2020-01-25 11:39:41 +0530

    Added:UC1 gambler make 1 bet win or loss done

--- shell.sh
+++ shell.sh
@@ -1,2 +1,22 @@
 #!/bin/bash -x
 echo "welcome to gambling simulation"
+INITIAL_STAKE=100;
+DAY=1;
+BET=1;
+WIN=1;
+LOSS=0;
+finalStake=INITIAL_STAKE;
+function GamblerSimulator(){
+
+	randomNumberBet=$((RANDOM%2))
+	if [[ $randomNumberBet -eq $1 ]]
+	then
+		finalStake=$(( $finalStake + 1 ))
+		return $finalStake
+	else
+		finalStake=$(( $finalStake - 1 ))
+		return $finalStake
+	fi
+}
+
+GamblerSimulator $(($WIN))
